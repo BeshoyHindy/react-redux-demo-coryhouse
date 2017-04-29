0757a94b62fffd063fa87fd1e716908d71909528
Beshoy Hindy <Beshoy.Hindy@gmail.com>
2017-04-29 23:28:42 +0200

    initials components

---
+++ src/components/App.js
@@ -0,0 +1,66 @@
+/**
+ * Created by BeshoyHindy on 4/27/2017.
+ */
+
+import React, {PropTypes} from 'react';
+import Header from './common/Header';
+// import {connect} from 'react-redux';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header/>
+        {/*<Header loading={this.props.loading} />*/}
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired,
+  // loading: PropTypes.bool.isRequired
+};
+
+// function mapStateToProps(state, ownProps) {
+//   return {
+//     loading: state.ajaxCallsInProgress > 0
+//   };
+// }
+//
+// export default connect(mapStateToProps)(App);
+
+export default App;
+
+
+/*// This component handles the App template used on every page.
+import React, {PropTypes} from 'react';
+import Header from './common/Header';
+import {connect} from 'react-redux';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header
+          loading={this.props.loading}
+        />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  };
+}
+
+export default connect(mapStateToProps)(App);*/
---
+++ src/components/common/Header.js
@@ -0,0 +1,43 @@
+import React, {PropTypes} from 'react';
+import {Link, IndexLink} from 'react-router';
+
+const Header = () => {
+  return (
+    <nav>
+      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      {" | "}
+      <Link to="/courses" activeClassName="active">Courses</Link>
+      {" | "}
+      <Link to="/about" activeClassName="active">About</Link>
+    </nav>
+  );
+};
+
+export default Header;
+
+
+
+
+/*
+import React, {PropTypes} from 'react';
+import {Link, IndexLink} from 'react-router';
+
+const Header = () => {
+  return (
+    <nav>
+      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      {" | "}
+      {/!*<Link to="/courses" activeClassName="active">Courses</Link>*!/}
+      {/!*{" | "}*!/}
+      <Link to="/about" activeClassName="active">About</Link>
+      {/!*{loading && <LoadingDots interval={100} dots={20}/>}*!/}
+    </nav>
+  );
+};
+
+// Header.propTypes = {
+//   loading: PropTypes.bool.isRequired
+// };
+
+export default Header;
+*/
