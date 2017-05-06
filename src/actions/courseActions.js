d327e50e794562371daad9844aca4492d6c85cac
Beshoy Hindy <Beshoy.Hindy@gmail.com>
2017-05-06 20:11:04 +0200

    apply 'Async Status and Error Handling' module

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 export function createCourse(course) {
   return {
@@ -31,9 +32,11 @@ export function updateCourseSuccess(course) {
 
 export function loadCourses() {
   return function (dispatch) {
+    dispatch(beginAjaxCall());
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error => {
+      dispatch(ajaxCallError(error));
       throw (error);
     });
   };
@@ -41,11 +44,11 @@ export function loadCourses() {
 
 export function saveCourse(course) {
   return function (dispatch, getState) {
-    // dispatch(beginAjaxCall());
+    dispatch(beginAjaxCall());
     return courseApi.saveCourse(course).then(course => {
       course.id ? dispatch(updateCourseSuccess(course)) : dispatch(createCourseSuccess(course));
     }).catch(error => {
-      // dispatch(ajaxCallError(error));
+      dispatch(ajaxCallError(error));
       throw(error);
     });
   };
